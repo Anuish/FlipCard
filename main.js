dd1f4bfee4c4abccf2a61273c26c7021a713ebbf
Modupeoluwa Oluwatayo <othanksgiving@gmail.com>
2021-01-30 13:31:22 +0000

    modified

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const card = document.querySelector('.card__inner');
 card.addEventListener('click', function(){
 	card.classList.toggle('is-flipped');
+	myFunction();
 });
+function myFunction(){
+	const x = document.querySelector('.card__face--front');
+	const y = document.querySelector('.card__face--back');
+	if(x.style.display == none){
+
+		y.style.display = block;
+	}
+	else{
+		x.style.display = block;
+	}
+
+}
